fa7c2030f0b2559bfb901f5663eb21e86b0e926f
Mildneey <90719292+Mildneey@users.noreply.github.com>
2021-09-25 23:08:13 +0700

    Update private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -51,9 +51,9 @@ Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
                         "➕ Tambahkan saya ke Grup Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
                 [
                     InlineKeyboardButton(
-                        "💬 Channel Updates", url=f"https://t.me/storeglitter"), 
+                        "💬 Channel Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                     InlineKeyboardButton(
-                        "🎈 Group Support", url=f"https://t.me/Virtuallnihboss")   
+                        "🎈 Group Support", url=f"https://t.me/{SUPPORT_GROUP}")   
                 ],[
                     InlineKeyboardButton(
                         "🎁 Donate", url=f"https://t.me/Biarenakliatnyaaa")
@@ -96,8 +96,8 @@ def map(pos):
         url = f"https://t.me/{SUPPORT_GROUP}"
         button = [
             [InlineKeyboardButton("➕ Tambahkan saya ke Grup Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-            [InlineKeyboardButton(text = '💬 Channel Updates', url=f"https://t.me/storeglitter"),
-             InlineKeyboardButton(text = '🔰 Group Support', url=f"https://t.me/Virtuallnihboss"),
+            [InlineKeyboardButton(text = '💬 Channel Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = '🔰 Group Support', url=f"https://t.me/{SUPPORT_GROUP}"),
             [InlineKeyboardButton(text = '«', callback_data = f"help+{pos-1}")]
         ]
     else:
